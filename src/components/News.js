2433e77442e0acc6aa5403e138cef5e87ccfe1a6
prashant_handel <prashanthandel2501@gmail.com>
2023-01-13 16:34:39 +0530

    Working on infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
+      page: 1,
+      pageSize: 9,
+      totalResults: 0
     };
     document.title = `News Fox | ${
       this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)
@@ -13,13 +17,16 @@ class News extends Component {
   }
 
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=3973f794321f41e0afb5170266ceaede`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=3973f794321f41e0afb5170266ceaede&page=${this.state.page}&pageSize=${this.state.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
+    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults });
   }
 
   render() {
+    const fetchData = async ()=>{
+      this.setState({page: this.state.page + 1})
+    }
     return (
       <div className="container my-3">
         <h1 className="text-center mb-5">
@@ -30,6 +37,17 @@ class News extends Component {
             Headlines
           </strong>
         </h1>
+        <InfiniteScroll
+          dataLength={this.state.articles.length} //This is important field to render the next data
+          next={fetchData}
+          hasMore={this.state.articles.length !== this.totalResults}
+          loader={<h4 className="text-center">Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
           <div className="row">
             {this.state.articles.map((element) => {
               return (
@@ -47,6 +65,7 @@ class News extends Component {
               );
             })}
           </div>
+        </InfiniteScroll>
       </div>
     );
   }
